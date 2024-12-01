daa8ccd43e7805c99d08dca610ff511bfae5f05b
chenzuoli <chenzuoli709@163.com>
2024-12-01 19:28:30 +0800

    重命名文件

--- multifilerename/RenameForm.Designer.cs
+++ multifilerename/RenameForm.Designer.cs
@@ -34,14 +34,15 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.listBox3 = new System.Windows.Forms.ListBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.rename_file = new System.Windows.Forms.Label();
             this.checkBox_numOrder = new System.Windows.Forms.CheckBox();
             this.checkBox_charOrder = new System.Windows.Forms.CheckBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rechoose_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -61,7 +62,7 @@
             this.listBox1.ItemHeight = 14;
             this.listBox1.Location = new System.Drawing.Point(149, 45);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(497, 592);
+            this.listBox1.Size = new System.Drawing.Size(497, 550);
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -71,12 +72,12 @@
             this.listBox3.ItemHeight = 14;
             this.listBox3.Location = new System.Drawing.Point(710, 45);
             this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(494, 592);
+            this.listBox3.Size = new System.Drawing.Size(494, 550);
             this.listBox3.TabIndex = 3;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(1235, 582);
+            this.button3.Location = new System.Drawing.Point(1235, 614);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(103, 23);
             this.button3.TabIndex = 4;
@@ -84,23 +85,15 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // label1
+            // rename_file
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 141);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 15);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "重命名文件夹";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 72);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 15);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "重命名文件";
+            this.rename_file.AutoSize = true;
+            this.rename_file.Location = new System.Drawing.Point(27, 72);
+            this.rename_file.Name = "rename_file";
+            this.rename_file.Size = new System.Drawing.Size(82, 15);
+            this.rename_file.TabIndex = 6;
+            this.rename_file.Text = "重命名文件";
+            this.rename_file.Click += new System.EventHandler(this.renamefile_Click);
             // 
             // checkBox_numOrder
             // 
@@ -111,6 +104,7 @@
             this.checkBox_numOrder.TabIndex = 10;
             this.checkBox_numOrder.Text = "按数字前缀";
             this.checkBox_numOrder.UseVisualStyleBackColor = true;
+            this.checkBox_numOrder.CheckedChanged += new System.EventHandler(this.checkBox_numOrder_CheckedChanged);
             // 
             // checkBox_charOrder
             // 
@@ -157,11 +151,21 @@
             ")",
             "-",
             "=",
+            "?英文",
+            "？中文",
+            "《",
+            "》",
+            ">",
+            "<",
+            "，",
+            "。",
+            "、",
             "空格"});
             this.checkedListBox1.Location = new System.Drawing.Point(1238, 192);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(100, 238);
+            this.checkedListBox1.Size = new System.Drawing.Size(100, 400);
             this.checkedListBox1.TabIndex = 15;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
             // 
             // label5
             // 
@@ -172,19 +176,39 @@
             this.label5.TabIndex = 16;
             this.label5.Text = "特殊字符删除";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(667, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 15);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "->";
+            // 
+            // rechoose_btn
+            // 
+            this.rechoose_btn.Location = new System.Drawing.Point(149, 613);
+            this.rechoose_btn.Name = "rechoose_btn";
+            this.rechoose_btn.Size = new System.Drawing.Size(125, 23);
+            this.rechoose_btn.TabIndex = 18;
+            this.rechoose_btn.Text = "重新选择文件夹";
+            this.rechoose_btn.UseVisualStyleBackColor = true;
+            this.rechoose_btn.Click += new System.EventHandler(this.rechoose_btn_Click);
+            // 
             // rename
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1371, 674);
+            this.Controls.Add(this.rechoose_btn);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.checkBox_charOrder);
             this.Controls.Add(this.checkBox_numOrder);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rename_file);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.listBox3);
@@ -204,14 +228,15 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ListBox listBox3;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label rename_file;
         private System.Windows.Forms.CheckBox checkBox_numOrder;
         private System.Windows.Forms.CheckBox checkBox_charOrder;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button rechoose_btn;
     }
 }
 
--- multifilerename/RenameForm.cs
+++ multifilerename/RenameForm.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Menu;
+using System.Net.Http;
 
 namespace multifilerename
 {
@@ -58,7 +60,6 @@ namespace multifilerename
 
         }
 
-
         private void checkBox_charOrder_CheckedChanged(object sender, EventArgs e)
         {
             List<string> newFiles = new List<string>();
@@ -103,37 +104,140 @@ namespace multifilerename
             }
         }
 
+        private void checkBox_numOrder_CheckedChanged(object sender, EventArgs e)
+        {
+            // 修改了数字前缀 checkbox
+            List<string> newFiles = new List<string>();
+
+            // 修改文件夹名称，按照数字前缀 添加
+            int initNum = 1;
+            if (checkBox_numOrder.Checked)
+            {
+                for (int i = 0; i < listBox3.Items.Count; i++)
+                {
+                    string newFileName = (i+initNum) + "_" + listBox3.Items[i].ToString();
+                    newFiles.Add(newFileName);
+                }
+                listBox3.Items.Clear();
+                newFiles.ForEach(item => listBox3.Items.Add(item));
+            }
+            else
+            {
+                for (int i = 0; i < listBox3.Items.Count; i++)
+                {
+                    // 将数字前缀删除
+                    string item = listBox3.Items[i].ToString();
+                    string prefix = (i + initNum) + "_";
+                    if (item.StartsWith(prefix))
+                    {
+                        string newFileName = item.Substring(prefix.Length);
+                        newFiles.Add(newFileName);
+                    }
+                }
+                listBox3.Items.Clear();
+                newFiles.ForEach(item => listBox3.Items.Add(item));
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 选中了其中一个，
+            List<object> inds = new List<object>();
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    inds.Add(checkedListBox1.Items[i]);
+                }
+            }
+
+            // 变更文件名
+            List<string> newFiles = new List<string>();
+            for (int i = 0; i < listBox3.Items.Count; i++)
+            {
+                // 按照选择的字符，从文件名中替换掉
+                string fileName = listBox3.Items[i].ToString();
+                for (int j = 0; j < inds.Count(); j++)
+                {
+                    string charItem = inds[j].ToString();
+                    Console.WriteLine("替换字符：" + charItem);
+
+                    if (charItem == "空格") charItem = " ";
+                    if (charItem == "?英文") charItem = "?";
+                    if (charItem == "？中文") charItem="？";
+                    fileName = fileName.Replace(charItem, "");
+                }
+                newFiles.Add(fileName);
+            }
+            listBox3.Items.Clear();
+            newFiles.ForEach(item => listBox3.Items.Add(item));
+        }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            // 检查文件是否有更改
+            if (!checkBox_numOrder.Checked && !checkBox_charOrder.Checked && checkedListBox1.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("未选择任何文件更改策略。");
+                return;
+            }
+
             // 确认修改按钮
             try
             {
-                for (int i = 0; i < listBox3.Items.Count; i++)
+                for (int i = 0; i < listBox1.Items.Count; i++)
                 {
                     // 如果目标文件存在，则删除
                     string fileOriPath = Path.Combine(folderBrowserDialog.SelectedPath, listBox1.Items[i].ToString());
                     string fileAbsPath = Path.Combine(folderBrowserDialog.SelectedPath, listBox3.Items[i].ToString());
-                    if (File.Exists(fileAbsPath))
-                    {
-                        File.Delete(fileAbsPath);
-                    }
+                    Console.WriteLine(fileOriPath);
+                    Console.WriteLine(fileAbsPath);
+
+                    // 如果删除目标文件，当文件名没有更改时，会把源文件删除
+                    //MessageBox.Show(fileOriPath + fileAbsPath);
+                    //if (File.Exists(fileAbsPath))
+                    //{
+                    //    File.Delete(fileAbsPath);
+                    //}
 
                     // move
-                    File.Move(fileOriPath, fileAbsPath);
+                    if (fileOriPath != fileAbsPath)
+                    {
+                        File.Move(fileOriPath, fileAbsPath);
+                    }
                 }
                 MessageBox.Show("修改成功。");
 
                 // 将修改置为失效，直到重新选择了文件夹
                 button3.Enabled = false;
+
+                // 使用记录
+                // string lezhisoft_statistic_url = "http://localhost:5000/admin/batch_rename";
+                string lezhisoft_statistic_url = "https://lezhifu.cc/admin/batch_rename";
+                using (var httpclient = new HttpClient())
+                {
+                    httpclient.GetAsync(lezhisoft_statistic_url).Wait();
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
-                MessageBox.Show(ex + "修改失败，请重试。");
+                MessageBox.Show(ex + " 修改失败，请重试。");
             }
         }
 
+        private void rechoose_btn_Click(object sender, EventArgs e)
+        {
+            // 重新选择文件夹
+            this.button1_Click(sender, e);
+        }
 
+        private void renamefile_Click(object sender, EventArgs e)
+        {
+            // 选择重命名文件 
+            this.listBox1.Items.Clear();
+            this.listBox3.Items.Clear();
+            this.button1.Visible = true;
+        }
     }
 }
